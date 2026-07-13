aca4254647d4fe131946f6c5bfca0a3d2779ff59
Blessing Mudarikwa <blessingmudarikwa23@gmail.com>
2026-07-13 16:08:35 +0200

    Add Inventory Management module

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
 
 from backend.app.db.database import Base, engine
+
+# Models
 from backend.app.models.user import User
 from backend.app.models.product import Product
+from backend.app.models.inventory import Inventory
+
+# Routers
 from backend.app.routers.user import router as user_router
 from backend.app.routers.product import router as product_router
 from backend.app.routers.auth import router as auth_router
+from backend.app.routers.inventory import router as inventory_router
+
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
@@ -16,12 +23,16 @@ app = FastAPI(
     version="1.0.0",
 )
 
+# Register Routers
 app.include_router(user_router)
-app.include_router(product_router)
 app.include_router(auth_router)
+app.include_router(product_router)
+app.include_router(inventory_router)
+
 
 @app.get("/")
 def home():
     return {
         "message": "Welcome to SmartChain Nexus API!"
     }
+    
