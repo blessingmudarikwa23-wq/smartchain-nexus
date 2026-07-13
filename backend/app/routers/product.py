07e350277d7aa38d648a158066686515e51db753
Blessing Mudarikwa <blessingmudarikwa23@gmail.com>
2026-07-13 13:56:54 +0200

    Add product creation endpoint

--- backend/app/routers/product.py
+++ backend/app/routers/product.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from backend.app.db.database import get_db
+from backend.app.models.product import Product
+from backend.app.schemas.product import ProductCreate, ProductResponse
 
 router = APIRouter(
     prefix="/products",
@@ -6,9 +11,32 @@ router = APIRouter(
 )
 
 
+@router.post("/", response_model=ProductResponse)
+def create_product(product: ProductCreate, db: Session = Depends(get_db)):
+
+    existing_product = db.query(Product).filter(Product.sku == product.sku).first()
+
+    if existing_product:
+        raise HTTPException(
+            status_code=400,
+            detail="SKU already exists."
+        )
+
+    new_product = Product(
+        product_name=product.product_name,
+        sku=product.sku,
+        category=product.category,
+        unit_price=product.unit_price,
+        quantity_in_stock=product.quantity_in_stock
+    )
+
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+
+    return new_product
+
+
 @router.get("/")
-def get_products():
-    return {
-        "message": "Products endpoint is working!",
-        "data": []
-    }
+def get_products(db: Session = Depends(get_db)):
+    return db.query(Product).all()
